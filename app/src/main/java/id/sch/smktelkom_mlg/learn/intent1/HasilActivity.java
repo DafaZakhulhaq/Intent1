3d48d263c2981456ea691b28cd274cf8d0112af8
Dafa Zakhulhaq <dafa27890@gmail.com>
2016-10-09 14:38:26 +0700

    StartActivityPart2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -3,6 +3,9 @@ package id.sch.smktelkom_mlg.learn.intent1;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class HasilActivity extends AppCompatActivity {
 
@@ -15,6 +18,18 @@ public class HasilActivity extends AppCompatActivity {
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String nama = getIntent().getStringExtra(MainActivity.NAMA);
+        int umur = getIntent().getIntExtra(MainActivity.UMUR, 0);
+
+        int yearsnow = Calendar.getInstance().get(Calendar.YEAR);
+        int tahunlahir = yearsnow - umur;
+
+        setTitle("Hasil");
+
+        TextView tvhasil = (TextView) findViewById(R.id.textview);
+        tvhasil.setText(nama + " lahir pada tahun " + tahunlahir);
+
     }
 
     @Override
